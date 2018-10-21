6b85c11a92da7618b1cb401793ffe300867fba0c
Marquin0 <k43lthas0@gmail.com>
2018-10-21 21:26:32 +0200

    lerp-follow

---
+++ Assets/Scripts/LerpFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpFollow : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 5f;
+    public bool keepOffset = false;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnEnable()
+    {
+        offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        var newPosition = Vector3.Lerp(transform.position, target.position, speed * Time.fixedDeltaTime);
+        if (keepOffset)
+            newPosition += offset;
+
+        transform.position = newPosition;
+	}
+}
