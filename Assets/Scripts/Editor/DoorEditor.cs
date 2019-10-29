c8c3272f2386c4eaaf38b82e723b8a7070d04ad1
Skilb <sebastian.kilb1@gmail.com>
2019-10-29 21:25:05 +0100

    interactable door

---
+++ Assets/Scripts/Editor/DoorEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Door))]
+public class DoorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Door myScript = (Door)target;
+        if (GUILayout.Button("Set Destination to current position"))
+        {
+            myScript.destination = myScript.doorObject.transform.localPosition;
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
