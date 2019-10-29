bc2d7e31b4741235f4753cedb9a848cf74e252a6
Skilb <sebastian.kilb1@gmail.com>
2019-10-29 22:06:40 +0100

    added moveable objects

---
+++ Assets/Scripts/Editor/MoveObjectEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MoveObject))]
+public class MoveObjectEditor : Editor
+{
+    [SerializeField]
+    MoveObject scriptTarget;
+
+    public void Awake()
+    {
+        scriptTarget = (MoveObject)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Set Clamp to current position"))
+        {
+            scriptTarget.destination = scriptTarget.transform.position;
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(target);
+            }
+        }
+    }
+}
