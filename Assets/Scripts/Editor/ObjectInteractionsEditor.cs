cd1511731f628e3c8467ce7aa5236b23459694d2
Skilb <sebastian.kilb1@gmail.com>
2019-10-29 22:28:06 +0100

    added switch trigger scripts, which triggers, when object or player collides with it

---
+++ Assets/Scripts/Editor/ObjectInteractionsEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ObjectInteractions))]
+public class ObjectInteractionsEditor : Editor
+{
+    [SerializeField]
+    ObjectInteractions scriptTarget;
+
+    public void Awake()
+    {
+        scriptTarget = (ObjectInteractions)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Set Clamp to current position"))
+        {
+            scriptTarget.Clamp = scriptTarget.transform.position;
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(target);
+            }
+        }
+    }
+}
