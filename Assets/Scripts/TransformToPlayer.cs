1385d2a59ae2444bc6303b22009ea62faea859f6
Skilb <sebastian.kilb1@gmail.com>
2021-10-16 22:29:39 +0200

    player and enemy interaction

---
+++ Assets/Scripts/TransformToPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformToPlayer : MonoBehaviour {
+
+	public GameObject player;
+	string tag;
+	public bool renameTagToPlayer = true;
+
+	// Use this for initialization
+	void Start () {
+		tag = gameObject.tag;
+	}
+
+	void OnEnable() {
+		if (renameTagToPlayer) {
+			gameObject.tag = "Player";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Fire2") == 1) {
+			TransformTOPlayer();
+		}
+	}
+
+	public void TransformTOPlayer() {
+		player.transform.position = transform.position;
+		player.SetActive(true);
+		gameObject.SetActive(false);
+	}
+}
