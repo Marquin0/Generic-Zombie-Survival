5b010313ef514d2e67abe00d31df7416ac89904d
Skilb <sebastian.kilb1@gmail.com>
2022-10-05 22:10:58 +0200

    laser wall implemented

---
+++ Assets/Scripts/LaserWall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserWall : MonoBehaviour {
+	
+	public Transform respawn;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player_Ball") {
+			other.transform.position = respawn.position;
+		}
+	}
+}
